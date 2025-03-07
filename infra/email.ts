f86a044a9f8fd23aff7eeefcaa7ddd1479d7e8b1
C H <charles.hewitt@pathwayanalytics.com>
2025-03-07 19:44:33 +0000

    wip: auth, fix: emailserver validation

--- infra/email.ts
+++ infra/email.ts
@@ -3,12 +3,12 @@
 const emailIdentity = `sst-auth@${process.env.DOMAIN || "example.com"}`;
 
 export const email = (() => {
-  const existingEmail = sst.aws.Email.get("EmailServer", emailIdentity);
+  const existingEmail = sst.aws.Email.isInstance(emailIdentity);
   if (existingEmail) {
     console.log(`Email identity ${emailIdentity} already exists.`);
     return existingEmail;
   } else {
-    console.log(`Creating email identity ${emailIdentity}.`);
+    console.log(`Creating email identity ${emailIdentity}, validation email sent.`);
     return new sst.aws.Email("EmailServer", {
       sender: emailIdentity,
     });
